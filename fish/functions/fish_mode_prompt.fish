aebe5ac3cd0cf2d9f4cb8ba6ac4ae062ae116d28
Iñigo Lopez de Heredia <inigo.lopezdeheredia@datadoghq.com>
2024-01-25 16:27:41 +0100

    update prompt

--- fish/functions/fish_mode_prompt.fish
+++ fish/functions/fish_mode_prompt.fish
@@ -1,16 +1,3 @@
 function fish_mode_prompt --description 'Displays the current mode'
-    # Do nothing if not in vi mode
-    if test "$fish_key_bindings" = "fish_vi_key_bindings"
-        switch $fish_bind_mode
-            case default
-                set_color white
-            case insert
-                set_color green
-            case visual
-                set_color magenta
-        end
-        echo '➜ '
-        set_color normal
-        echo -n ' '
-    end
+    # Do nothing, use fish_prompt instead
 end
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -8,10 +8,24 @@ function fish_prompt
   end
 
   set_color $fish_color_cwd
-  printf '%s' (prompt_pwd)
+  echo -n (prompt_pwd)
   set_color normal
 
-  printf '%s ' (fish_git_prompt)
+  echo -n (fish_git_prompt)
 
+  set_color normal
+
+  if test "$fish_key_bindings" = "fish_vi_key_bindings"
+      switch $fish_bind_mode
+          case default
+              set_color white
+          case insert
+              set_color green
+          case visual
+              set_color magenta
+      end
+  end
+
+  echo -n '> '
   set_color normal
 end
