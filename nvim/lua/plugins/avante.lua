db80f83ecdb149d4fb9f8f808a158946e8d5f274
Iñigo Lopez de Heredia <inigo.lopezdeheredia@datadoghq.com>
2025-06-07 13:40:45 +0200

    Don't use markdown plugin outside Avante

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -54,11 +54,10 @@ return
     {
       -- Make sure to set this up properly if you have lazy=true
       "MeanderingProgrammer/render-markdown.nvim",
-      enabled = false,
       opts = {
-        file_types = { "markdown", "Avante" },
+        file_types = { "Avante" },
       },
-      ft = { "markdown", "Avante" },
+      ft = { "Avante" },
     },
   },
 }
