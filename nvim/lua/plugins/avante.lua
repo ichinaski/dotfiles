20f36b497ddd5b72cdacb7f756077f3a81a7870e
Iñigo Lopez de Heredia <inigo.lopezdeheredia@datadoghq.com>
2025-06-07 13:57:19 +0200

    clean up avante

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -24,15 +24,15 @@ return
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
     -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
-    -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    "stevearc/dressing.nvim",
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
     -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
     -- "ibhagwan/fzf-lua", -- for file_selector provider fzf
-    -- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     -- "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
       -- support for image pasting
